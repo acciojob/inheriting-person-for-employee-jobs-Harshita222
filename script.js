3667f3a8a34d42c5102fa2f3f21e87f1239727d8
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-13 18:45:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// class Person{
+// 	constructor(name, age) {
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+// 	greet(){
+// 		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+// 	}
+// }
+
+// class Employee extends Person{
+// 	constructor(name, age, jobTitle) {
+// 		super(name,age);
+// 		this.jobTitle = jobTitle;
+// 	}
+//    jobGreet(){
+// 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+	   
+//    }
+// }
+// // Do not change code below this line
+// window.Person = Person;
+// window.Employee = Employee;
+
 class Person{
 	constructor(name, age) {
 		this.name = name;
@@ -14,8 +38,7 @@ class Employee extends Person{
 		this.jobTitle = jobTitle;
 	}
    jobGreet(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
-	   
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}`);
    }
 }
 // Do not change code below this line
